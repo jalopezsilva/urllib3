8d6d1ad38576b3aacae5ee3a13ee883cd9933fc9
Jorge Lopez Silva <jalopezsilva@gmail.com>
2020-02-24 16:19:00 -0800

    Add SSLTransport to support TLS in TLS connections.
    TLS within TLS is not supported easily within the ssl library. The
    SSLSocket actually takes over the existing socket
    (https://github.com/python/cpython/blob/master/Lib/ssl.py#L999-L1006)
    instead of wrapping it entirely. The only way to support to TLS within
    TLS is with the wrap_bio methods.
    
    This commit introduces SSLTransport which wraps a socket in TLS using
    the provided SSL context. Rather than taking over the socket it uses the
    wrap_bio methods to perform TLS on top of that socket.

---
+++ test/contrib/test_ssltransport.py
@@ -0,0 +1,9 @@
+# from dummyserver.testcase import SocketDummyServerTestCase, consume_socket
+# from dummyserver.server import (
+#     DEFAULT_CERTS,
+#     DEFAULT_CA,
+#     get_unreachable_address,
+#     encrypt_key_pem,
+# )
+
+# import pytest
